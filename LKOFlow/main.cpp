a5e39b342632e29ab53d9f84a5ca6544bdd42fe1
zhangxiaoya <zhangxiaoya1989@gmail.com>
2017-04-12 09:39:46 +0800

    test read two gray image

--- LKOFlow/main.cpp
+++ LKOFlow/main.cpp
@@ -8,11 +8,31 @@ using namespace std;
 
 int main()
 {
-	Mat image = imread("timg.jpg");
-	if (image.data)
+	Mat img1 = imread("1.jpg");
+	Mat img2 = imread("2.jpg");
+	Mat graImg1;
+	Mat graImg2;
+
+	if (img1.data)
 	{
-		imshow("test Image show", image);
+		imshow("Image1", img1);
+		cout << img1.channels() << endl;
+		cout << img1.rows << endl;
+		cout << img1.cols << endl;
+		cvtColor(img1, graImg1, CV_BGR2GRAY);
+		cout << graImg1.channels() << endl;
 		waitKey(0);
-		destroyAllWindows();
 	}
+
+	if (img2.data)
+	{
+		imshow("Image2", img2);
+		cout << img2.channels() << endl;
+		cvtColor(img2, graImg2, CV_BGR2GRAY);
+		cout << graImg2.channels() << endl;
+		waitKey(0);
+	}
+
+	destroyAllWindows();
+	return 0;
 }
